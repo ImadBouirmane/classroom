b239760c02b56af4c9d15d69e1d0c8e82e1c1f62
Mark Tareshawty <mark@marktareshawty.com>
2015-10-05 14:15:18 -0400

    copy and rubocop

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -2,10 +2,11 @@ module StarterCode
   include GitHub
   extend ActiveSupport::Concern
 
+  # rubocop:disable MethodLength
   def starter_code_repository_id(repo_name)
     return unless repo_name.present?
 
-    if repo_name =~ /[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+/i
+    if repo_name =~ %r{[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+}
       begin
         github_repository = GitHubRepository.new(current_user.github_client, nil)
         github_repository.repository(repo_name).id
@@ -13,7 +14,8 @@ module StarterCode
         raise GitHub::Error, err.message
       end
     else
-      raise GitHub::Error, 'Invalid repository format'
+      fail GitHub::Error, 'Invalid repository name, use the format owner/name'
     end
   end
+  # rubocop:enable MethodLength
 end
