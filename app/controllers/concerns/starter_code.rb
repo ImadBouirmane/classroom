79b055e279d648fa9f9a0fd8e05cbcf7d95cbf84
Mark Tareshawty <mark@marktareshawty.com>
2015-10-05 10:14:05 -0400

    expect a certain format for starter code

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -5,11 +5,15 @@ module StarterCode
   def starter_code_repository_id(repo_name)
     return unless repo_name.present?
 
-    begin
-      github_repository = GitHubRepository.new(current_user.github_client, nil)
-      github_repository.repository(repo_name).id
-    rescue ArgumentError => err
-      raise GitHub::Error, err.message
+    if repo_name =~ /[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+/i
+      begin
+        github_repository = GitHubRepository.new(current_user.github_client, nil)
+        github_repository.repository(repo_name).id
+      rescue ArgumentError => err
+        raise GitHub::Error, err.message
+      end
+    else
+      raise GitHub::Error, 'Invalid repository format'
     end
   end
 end
